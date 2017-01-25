f88672ebf87cd900fab0fef440d60dc274386383
geneh96 <geneh96@gmail.com>
2017-01-25 14:18:45 -0500

    Add button..
    In the mainactivity added a button that increments as you click on it

--- app/src/main/java/com/example/gene/week2_demo2/MainActivity.java
+++ app/src/main/java/com/example/gene/week2_demo2/MainActivity.java
@@ -2,12 +2,18 @@ package com.example.gene.week2_demo2;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    int count=0;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void m0nClick(View view) {
+        ((Button)view).setText("Clicked " + (++count));
     }
 }
